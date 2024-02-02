b47adaf31e12a1cc4f88030e7fdc47afab3fc612
Магжан Куанышбек <Kuanyshbek.M@ktp.loc>
2024-02-02 17:38:03 +0600

    Adding Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router"
 import Home from "./components/Home"
 import Header from "./feautures/header/Header"
+import Footer from "./feautures/footer/Footer"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   )
 }
---
+++ src/feautures/footer/Footer.jsx
@@ -0,0 +1,44 @@
+import { Box, Container, Image, Text } from "@chakra-ui/react";
+import { GrInstagram } from "react-icons/gr";
+import { BiLogoTelegram } from "react-icons/bi";
+import { FaWhatsapp } from "react-icons/fa";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <>
+      <Container
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        padding={2}
+        minWidth={{
+          md: "700px",
+          lg: "900px",
+          xl: "1100px",
+        }}
+      >
+        <Image src={logo} height="60px"/>
+        <Text>Copyright © ANDYZ CORP All Right Reserved</Text>
+        <Box 
+        display={"flex"}
+        justifyContent={"space-between"}
+        width={{
+            base: "100px",
+            md: "120px",
+            lg: "140px",
+            xl: "160px",
+  
+        }}
+        >
+          <Link><BiLogoTelegram fontSize={25}/></Link>
+          <Link><GrInstagram fontSize={25}/></Link>
+          <Link><FaWhatsapp fontSize={25}/></Link>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default Footer;
